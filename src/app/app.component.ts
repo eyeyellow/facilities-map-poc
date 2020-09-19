e6099276e533b8f9d5a1fa10d56d18e00f8afedd
Frank Aiello <frankvaiello@gmail.com>
2020-09-19 11:57:35 -0400

    Removes auto-generated angular html and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>Hello template</div>
+  `,
 })
 export class AppComponent {
   title = 'facilities-map-poc';
