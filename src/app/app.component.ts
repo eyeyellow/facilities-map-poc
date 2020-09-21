b6ea1ec95562660460cefcc87b605335951df713
Frank Aiello <frankvaiello@gmail.com>
2020-09-21 11:22:19 -0400

    Adds amenity popup content to map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { tileLayer, latLng, Map, marker, Layer, featureGroup, icon, Marker } from 'leaflet';
 
-import { AmenitiesIfc, FacilitiesService } from './facilities.service';
+import { AmenitiesIfc, AttributesIfc, FacilitiesService } from './facilities.service';
 
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
@@ -17,11 +17,20 @@ Marker.prototype.options.icon = iconDefault;
 const DEFAULT_ZOOM = 2;
 const DEFAULT_CENTER = latLng(26.983785585, -121.018025935);
 
+function popupContent(attributes: AttributesIfc): string {
+  return `
+    <h3>${attributes.Name}</h3>
+    <hr />
+    <p><strong>Access: </strong>${attributes.Public_Use}</p>
+    <p><strong>Wheelchair Accessible: </strong>${attributes.Han_Access}</p>
+  `;
+}
+
 @Component({
   selector: 'app-root',
   template: `
     <div class="container">
-      <div leaflet style="height: 500px;" 
+      <div leaflet style="height: 500px;"
           [leafletOptions]="options"
           [leafletLayers]="layers"
           (leafletMapReady)="onMapReady($event)"
@@ -41,7 +50,7 @@ const DEFAULT_CENTER = latLng(26.983785585, -121.018025935);
 export class AppComponent {
   title = 'facilities-map-poc';
   _map: Map;
-  layers: Layer[];
+  layers: Layer[] = [];
   options = {
       layers: [
           tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -56,12 +65,17 @@ export class AppComponent {
 
   ngOnInit() {
     this.facilities.getAmenities(31521).subscribe((amenities:AmenitiesIfc[]) => {
-      // TODO - filter out the layer that has the coords of the reserve itself (or just no title)
-      this.layers = amenities.map((amenity) => {
-        return marker(
+      amenities.forEach((amenity) => {
+        // Filter out any amenity that doesn't have a name
+        if (!amenity.attributes.Name.trim()) return;
+        let markerLayer: Layer;
+        markerLayer = marker(
           [amenity.geometry.y, amenity.geometry.x],
           { title: amenity.attributes.Name }
         )
+        // Add popup with amenity info
+        markerLayer.bindPopup(popupContent(amenity.attributes));
+        this.layers.push(markerLayer);
       })
       // TODO - eventually add clusters to the map
       this._map.fitBounds(featureGroup(this.layers).getBounds().pad(.1));
--- src/app/facilities.service.ts
+++ src/app/facilities.service.ts
@@ -5,7 +5,7 @@ import { map } from 'rxjs/operators';
 
 const BASE_URL = 'https://services.arcgis.com/QVENGdaPbd4LUkLV/arcgis/rest/services/FWS_National_Visitor_Service_Amenities_View/FeatureServer/0/query';
 
-interface AttributesIfc {
+export interface AttributesIfc {
   Han_Access: string,
   Name: string,
   Public_Use: string
